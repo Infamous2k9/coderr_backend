6b90a75a76607132cd4fa8f4a5257110549a4a7c
Andre Funk <Andre.funk.94@web.de>
2026-09-11 12:50:51 +0200

    feat: Profile model added und update the requirements.txt

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    TYPE_CHOICES = [
+        ("customer", "Customer"),
+        ("business", "Business"),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+    file = models.ImageField(upload_to="profiles/", blank=True, null=True)
+    location = models.CharField(max_length=255, blank=True, default="")
+    tel = models.CharField(max_length=50, blank=True, default="")
+    description = models.CharField(max_length=500, blank=True, default="")
+    working_hours = models.CharField(max_length=100, blank=True, default="")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} ({self.type})"
