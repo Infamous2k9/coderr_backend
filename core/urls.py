eb7271d7a9d815cac82ec25c6decd0eb93ec869c
Andre Funk <Andre.funk.94@web.de>
2026-09-21 13:18:01 +0200

    fix: urls for media added

--- core/urls.py
+++ core/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -29,3 +31,5 @@ urlpatterns = [
     path("api/", include("reviews.urls")),
     path("api/base-info/", base_info, name="base-info"),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
