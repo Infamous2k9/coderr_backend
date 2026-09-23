a4b1e6d05004b4bee911c9838c342d85c2d2d759
Andre Funk <Andre.funk.94@web.de>
2026-09-23 13:42:53 +0200

    fix: validators dont ignore empty string

--- offers/validators.py
+++ offers/validators.py
@@ -5,7 +5,7 @@ def validate_numeric_query_params(query_params, param_names):
     """Return an error dict if any of the given params is not a valid number, else None."""
     for param in param_names:
         value = query_params.get(param)
-        if value is not None:
+        if value:
             try:
                 float(value)
             except ValueError:
